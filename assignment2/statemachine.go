b25c71ecebb65618cc5fcd01176bc65caa208db0
Ratheeshkv <ratheeshkv179@gmail.com>
2016-02-22 06:32:26 +0530

    first comit

--- assignment2/statemachine.go
+++ assignment2/statemachine.go
@@ -1,14 +1,13 @@
 package sm
 import (
-		"fmt"
+ //"fmt"
 	   )
 
-var eventChannel = make(chan interface{},5)
-var actionChannel = make(chan interface{},5)
-var clientChannel  = make (chan interface{},5)
+var eventChannel = make(chan interface{},5)  // all events  timeout, vote req.reply, append req/rply pushed here
+var actionChannel = make(chan interface{},5) // response events from server machine pushed here
+var clientChannel  = make (chan interface{},5) // append data, commit pushed here
 
 type VoteReqEv struct {
-
 	term int
 	candidateId int
 	lastLogIndex int
@@ -16,7 +15,6 @@ type VoteReqEv struct {
 }
 
 type VoteRespEv struct {
-
 	term int
 	voteGranted bool
 }
@@ -28,65 +26,62 @@ type Entry struct {
 }
 
 type Timeout struct {
-
 }
 
 type Append struct {
-
-	data [1] byte
+	data string
 }
 
 type Commit struct {
 	index int
-	data [] byte
+	data [] Entry
 	err string
 }
 
 type Alarm struct {
-
+	t int // time after t millisec, timeout will generated
 }
-
 
 type Send struct {
 	id int
 	event interface{}
 }
+
 type Logstore struct {
 	index int
-	data [] byte
+	data [10] Entry
 
 }
 
 type AppendEntriesReqEv struct {
-
 	term int
 	leaderId int
 	prevLogIndex int
 	prevLogTerm int
-	logEntries[100] byte
+	logEntries[10] Entry
+	lengthOfLogEnty int // length of log entry 
 	leaderCommit int
-	// etc
 }
 
 type AppendEntriesRespEv struct{
 
-	term int
-	success bool	
+	id int // serverID who generated this response
+	term int // term numner
+	success bool	// 
 }
-
 
 type StateMachine struct {
 
-	id int // server id
+	id int 		 // this server id
 	leaderId int // leader id of the cluster
 	state string // state of stateMachine LEADER, CANDIDATE or FOLLOWER
 	peers []int  // other server ids
 	currentTerm int 
-	votedFor int
-	voteCount int
+	votedFor map[int] int//make(map[int]int) // candidateID vs voted or not
+	voteCount [10]int
 	replicateCount int
-	log[100] byte
-	logLastIndex int
+	log[10] Entry
+	logLastIndex int 
 	logLastTerm int
 	commitIndex int
 	lastApplied int
@@ -98,9 +93,7 @@ type StateMachine struct {
  
 func PutEventToChannel( ev interface{}){
 	//eventChannel <- ev
-	
 	switch ev.(type) {
-
 		case Commit:
 			clientChannel <- ev
 		default:
@@ -108,20 +101,15 @@ func PutEventToChannel( ev interface{}){
 		}
 }
 
-func (sm *StateMachine) RestartStateMachine(id int){
-	sm.state = "FOLLOWER"
-}
-
+ 
 func  GetResponseEventFromChannel() interface{}{
-	
-	var msg interface{}
 	select {
-     case msg := <- clientChannel:
+     case msg := <- clientChannel: 
 	   	return msg
      case msg := <- eventChannel:
      	 return msg
 	 }
-	 return msg
+	 //msg = nil
 }
 
 func (sm *StateMachine) ProcessEvent (ev interface{}) {
@@ -129,28 +117,31 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 	switch ev.(type) {
 	case Append:
 		cmd := ev.(Append)
-		
+
 		if(sm.state == "LEADER"){
 			// append to log and send append request to all
-			sm.logLastIndex = sm.logLastIndex + 1
-			sm.log[sm.logLastIndex] = cmd.data[0]
-			sm.voteCount = 0
+			sm.logLastIndex = sm.logLastIndex + 1 // increment log index
+			sm.log[sm.logLastIndex] = Entry{term:sm.currentTerm,command:cmd.data} // append to leader's log
+
 
 			   event := AppendEntriesReqEv{
 						term:sm.currentTerm,
 						leaderId:sm.id,
 						prevLogIndex:sm.logLastIndex-1,
-						prevLogTerm:0,
-						logEntries:sm.log,
+						prevLogTerm:sm.log[sm.logLastIndex-1].term,
+					//	logEntries:sm.log[sm.logLastIndex:],
 						leaderCommit:sm.commitIndex}
+
+				copy(event.logEntries[:],sm.log[sm.logLastIndex:])
 
 			for i:= 1; i<= len(sm.peers);i++ {
 				go PutEventToChannel(Send{sm.peers[i-1],event})
 			}
+
  
-			}else if(sm.state == "CANDIDATE" || sm.state == "FOLLOWER"){
+			}else if(sm.state == "FOLLOWER"){
 				// redirect to leader
-				if(sm.leaderId != 0){
+				if(sm.leaderId != 0){               // know the leaderID
 					go PutEventToChannel(Send{sm.leaderId,cmd})					
 				}
 			}
@@ -159,71 +150,81 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 	case AppendEntriesReqEv:
 		cmd := ev.(AppendEntriesReqEv)
 
-		if(sm.state == "LEADER"){
+		// my term is higher
+		if(cmd.term < sm.currentTerm){
+			   event := AppendEntriesRespEv{
+						term:sm.currentTerm,
+						success:false}
+			   go PutEventToChannel(Send{cmd.leaderId,event})
+			   break
+		} 
 
-			if(cmd.term > sm.currentTerm){ // new leader/ leader with higher term exist
-				sm.state = "FOLLOWER"
-				sm.currentTerm = cmd.term  
- 				// append entries to the log
- 				// recursively update the log
-				actionChannel  <- AppendEntriesRespEv{ term:sm.currentTerm, success : true}
-			} else if(cmd.term == sm.currentTerm){
+		if(cmd.lengthOfLogEnty == 0) {
+			// heart beat from leader
+			//refresh the election timeout
+			// create an alarm, which Timeout after t milliseconds.
+			event := Alarm{t:200}
+			go PutEventToChannel(event)
 
+		} else {
+
+		if( sm.currentTerm < cmd.term){
+			sm.currentTerm = cmd.term
+			sm.state = "FOLLOWER"
+		}
+
+		// not a heartbeat, it is an actuall append request
+		sm.leaderId = cmd.leaderId // got leaderID
+		// no entry for particular index, my log is old
+		if (sm.logLastIndex < cmd.prevLogIndex) {
+			   event := AppendEntriesRespEv{
+			   			id:sm.id,
+						term:sm.currentTerm,
+						success:false}
+			   go PutEventToChannel(Send{cmd.leaderId,event})
+			   break
+		}
+
+		// no matching entries in log (matching index and term)
+		if ((sm.logLastIndex >= cmd.prevLogIndex) && (sm.log[cmd.prevLogIndex].term != cmd.prevLogTerm)){
+			   			event := AppendEntriesRespEv{
+			   			id:sm.id,
+						term:sm.currentTerm,
+						success:false}
+			   go PutEventToChannel(Send{cmd.leaderId,event})
+			   break
+		} 
+		// conflict with new entry
+		if ((sm.logLastIndex >= (cmd.prevLogIndex+1)) && (sm.log[(cmd.prevLogIndex)+1].term != cmd.logEntries[0].term)){
+			   			sm.logLastIndex = cmd.prevLogIndex // ignore all conflicting entries		   
+		}
+		// append new entries 
+		if ((sm.logLastIndex == cmd.prevLogIndex) && (sm.log[sm.logLastIndex].term == cmd.prevLogTerm)){
+			   			for i:= sm.logLastIndex+1 ;i<=cmd.lengthOfLogEnty ;i++ {
+//			   				fmt.Print("ONE",cmd.logEntries[i])
+			   				sm.log[i] = cmd.logEntries[i]
+			   			}
+			   			sm.logLastIndex = sm.logLastIndex + (cmd.lengthOfLogEnty-sm.logLastIndex)
+			   			event := AppendEntriesRespEv{
+			   							id:sm.id,
+										term:sm.currentTerm,
+										success:true}
+			   			go PutEventToChannel(Send{cmd.leaderId,event})
+			   			
+		}
+
+		if(cmd.leaderCommit > sm.commitIndex) {
+			if( cmd.leaderCommit < sm.logLastIndex){
+				sm.commitIndex = cmd.leaderCommit 
 			} else {
-
-				// reject the request
-				actionChannel  <- AppendEntriesRespEv{ term:sm.currentTerm, success : false}
-
+				sm.commitIndex = sm.logLastIndex 
 			}
-
-			}else if(sm.state == "CANDIDATE"){
-				if(cmd.term > sm.currentTerm){
-					sm.state = "FOLLOWER"
-					sm.currentTerm = cmd.term  
- 					// append entries to the log
- 					// recursively update the log
-
-			/*		if(sm.logLastIndex<cmd.prevLogIndex || ((sm.logLastIndex == cmd.prevLogIndex) && (sm.logLastTerm != cmd.prevLogTerm)){
-						go PutEventToChannel(AppendEntriesRespEv{ term:sm.currentTerm, success : false})						
-					}
-			*/
-
-
-
-
-
-				}else{
-					// reject the request
-				//	 response = GetResponseEventFromChannel()   			
-					//fmt.Print("\nRES#",response,"#\n")
-				//ionChannel  <- AppendEntriesRespEv{ term:sm.currentTerm, success : false}
-				}
-				// heartbeat 
-				if(len(cmd.logEntries) == 0){
-					//refresh the timer
-				}
-
-
-
-			}else if (sm.state == "FOLLOWER"){
-
-
-			if(cmd.term > sm.currentTerm){ // new leader/ leader with higher term exist
-				sm.currentTerm = cmd.term 
-			}
-
-			// check for heartbeat
-
-			}
-
-
-
+		}	
+	}
 
 	case AppendEntriesRespEv:
 		cmd := ev.(AppendEntriesRespEv)
-
 		if(sm.state == "LEADER"){
-
 			if(cmd.term > sm.currentTerm){ // new leader/ leader with higher term exist
 				sm.state = "FOLLOWER"
 				sm.currentTerm = cmd.term  
@@ -233,17 +234,33 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 				if(cmd.success){ //
 					// increment count of majority
 					sm.replicateCount = sm.replicateCount + 1
-					if(sm.replicateCount >= 3) {		
-					if(sm.commitIndex < sm.logLastIndex) {
-						sm.commitIndex = sm.logLastIndex
-						event := Commit {index:sm.commitIndex, data:sm.log[sm.logLastIndex:] , err:""}
-						go PutEventToChannel(event)
-					}			
-					}
-			}
-			}
-			
+					if(sm.replicateCount == 3) {	
 
+					if(sm.commitIndex < sm.logLastIndex) { // got majority vote from peers to commit the data
+
+						sm.commitIndex = sm.logLastIndex  // update commit index
+						sm.nextIndex[cmd.id] = sm.logLastIndex+1
+						sm.matchedIndex[cmd.id] = sm.logLastIndex
+
+
+						event := Commit {index:sm.commitIndex, data:sm.log[:sm.logLastIndex] , err:""}
+						go PutEventToChannel(event)
+					}}
+				}else { // log nconsistency, retry AppenedEntryRPC request 
+
+						sm.nextIndex[cmd.id] = sm.nextIndex[cmd.id] - 1
+						event:= AppendEntriesReqEv{
+									term:sm.currentTerm,
+									leaderId:sm.id,
+									prevLogIndex:sm.logLastIndex-1,
+									prevLogTerm:sm.log[sm.logLastIndex-1].term,
+									lengthOfLogEnty:1,
+									leaderCommit:sm.commitIndex}
+
+							go PutEventToChannel(Send{cmd.id,event})
+
+				}
+			}
 		}else if(sm.state == "CANDIDATE"){
 
 				if(cmd.term > sm.currentTerm){ // new leader/ leader with higher term exist
@@ -261,27 +278,28 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 
 	case VoteReqEv:
 		cmd := ev.(VoteReqEv)
-		fmt.Print("\n",cmd.term, sm.currentTerm)
+		//fmt.Print("\n",cmd.term, sm.currentTerm)
 
 			if(cmd.term < sm.currentTerm){
-				go PutEventToChannel(VoteRespEv{term : sm.currentTerm , voteGranted: false})
+				go PutEventToChannel(Send{cmd.candidateId,VoteRespEv{term : sm.currentTerm , voteGranted: false}})
 			}else {
 
 			if(cmd.term > sm.currentTerm){
 				sm.currentTerm = cmd.term
-				sm.votedFor = 0
+				sm.state = "FOLLOWER"
+				sm.votedFor[cmd.term] = 0 // first time seeing, not voted
 			}
 
-			if(sm.votedFor == 0 || sm.votedFor == sm.id){
+			if(sm.votedFor[cmd.term] == 0 || sm.votedFor[cmd.term] == sm.id){
 				if((sm.logLastTerm  > cmd.lastLogTerm) ||
 					(sm.logLastTerm  == cmd.lastLogTerm && sm.logLastIndex > cmd.lastLogIndex)){
-					go PutEventToChannel(VoteRespEv{term : sm.currentTerm , voteGranted: false})		
+					go PutEventToChannel(Send{cmd.candidateId,VoteRespEv{term : sm.currentTerm , voteGranted: false}})		
 				}else{
-					sm.votedFor = cmd.candidateId
-					go PutEventToChannel(VoteRespEv{term : sm.currentTerm , voteGranted: true})		
+					sm.votedFor[cmd.term] = cmd.candidateId //voted
+					go PutEventToChannel(Send{cmd.candidateId,VoteRespEv{term : sm.currentTerm , voteGranted: true}})		
 				}
-			}else {
-					go PutEventToChannel(VoteRespEv{term : sm.currentTerm , voteGranted: false})		
+			}else { // already voted for this term
+					go PutEventToChannel(Send{cmd.candidateId,VoteRespEv{term : sm.currentTerm , voteGranted: false}})		
 				  }
 			}
 
@@ -300,25 +318,31 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 			if(cmd.term > sm.currentTerm){ // new leader/ leader with higher term exist
 				sm.state = "FOLLOWER"
 				sm.currentTerm = cmd.term  
-			} else	if(cmd.voteGranted){
-			sm.voteCount = sm.voteCount + 1 // increment votecount
-			if(sm.voteCount >=3){
-				sm.state = "LEADER"
-				// send heartbeat msg
-				// for all other servers
-			   event := AppendEntriesReqEv{
-						term:sm.currentTerm,
-						leaderId:sm.id,
-						prevLogIndex:sm.logLastIndex-1,
-						prevLogTerm:sm.logLastTerm,
-						logEntries:[100] byte{},
-						leaderCommit:sm.commitIndex}
 
-			for i:= 1; i<= len(sm.peers);i++ {
-				go PutEventToChannel(Send{sm.peers[i-1],event})
-			}
-				}
-			}
+			} else if(cmd.term < sm.currentTerm){
+				// ignore the msg packet
+			}else {
+				if(cmd.voteGranted){
+					sm.voteCount[sm.currentTerm] = sm.voteCount[sm.currentTerm] + 1 // increment votecount
+				
+					if(sm.voteCount[sm.currentTerm] == 3){
+						sm.state = "LEADER"
+						// send heartbeat msg
+						// for all other servers
+			    		event := AppendEntriesReqEv{
+									term:sm.currentTerm,
+									leaderId:sm.id,
+									prevLogIndex:sm.logLastIndex-1,
+									prevLogTerm:sm.log[sm.logLastIndex-1].term,
+									lengthOfLogEnty:0,
+									leaderCommit:sm.commitIndex}
+
+						for i:= 1; i<= len(sm.peers);i++ {
+						//	fmt.Print("\nsending : ",i+1,event)
+							go PutEventToChannel(Send{sm.peers[i-1],event})
+						}
+					}
+			}}
 		}else if (sm.state == "FOLLOWER"){
 			if(cmd.term > sm.currentTerm){ // update the term
 				sm.state = "FOLLOWER"
@@ -330,13 +354,14 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 	case Timeout:
 
 		if(sm.state == "LEADER"){
- 
+ 				//heartbeat timeout	
 			   event := AppendEntriesReqEv{
 						term:sm.currentTerm,
 						leaderId:sm.id,
 						prevLogIndex:sm.logLastIndex-1,
-						prevLogTerm:0,
-						logEntries:[100] byte{},
+						prevLogTerm:sm.log[sm.logLastIndex-1].term,
+						lengthOfLogEnty:0,
+						//logEntries:[100] byte{},
 						leaderCommit:sm.commitIndex}
 
 			for i:= 1; i<= len(sm.peers);i++ {
@@ -347,13 +372,14 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 				// election timeut
 				sm.currentTerm = sm.currentTerm + 1
 				//sm.state = "CANDIADTE"
-				sm.votedFor = sm.id
+				sm.votedFor[sm.currentTerm] = sm.id
+				sm.voteCount[sm.currentTerm] = 0
 
-			   event := VoteReqEv{
+			   	event := VoteReqEv{
 			   					term:sm.currentTerm,
 			   					candidateId:sm.id,
 			   					lastLogIndex:sm.logLastIndex,
-			   					lastLogTerm:sm.logLastTerm}
+			   					lastLogTerm:sm.log[sm.logLastIndex].term}
 
 			for i:= 1; i<= len(sm.peers);i++ {
 				go PutEventToChannel(Send{sm.peers[i-1],event})
@@ -363,26 +389,20 @@ func (sm *StateMachine) ProcessEvent (ev interface{}) {
 				// election timeout
 				sm.currentTerm = sm.currentTerm + 1
 				sm.state = "CANDIDATE"
-				sm.votedFor = sm.id
+				sm.votedFor[sm.currentTerm] = sm.id
+				sm.voteCount[sm.currentTerm] = 0
 
-				event := AppendEntriesReqEv{
-						term:sm.currentTerm,
-						leaderId:sm.id,
-						prevLogIndex:sm.logLastIndex-1,
-						prevLogTerm:0,
-						logEntries:[100] byte{}, // empty data entry
-						leaderCommit:sm.commitIndex}
+				
+				event := VoteReqEv{
+			   					term:sm.currentTerm,
+			   					candidateId:sm.id,
+			   					lastLogIndex:sm.logLastIndex,
+			   					lastLogTerm:sm.log[sm.logLastIndex].term}
 
 				for i:= 1; i<= len(sm.peers);i++ {
 					go PutEventToChannel(Send{sm.peers[i-1],event})
 				}
-				// to all other servers
-				//actionChannel <- VoteReqEv{}
 			}
-
-		//fmt.Printf("%v\n", cmd)
-
-	default: println ("Unrecognized")
 	}
 }
 
